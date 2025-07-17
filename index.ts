e8bca6022d842107a48927e7c6d4052ad60b1971
yurika-24hour <fiore.della.canzone.di.amore@gmail.com>
2025-07-17 17:14:53 +0900

    LILIY_section1-1課題提出

---
+++ index.ts
@@ -0,0 +1,55 @@
+/*1. 数値の配列から最大値を取得する関数を作成してください。
+
+- 10個以上の要素を持つ数値型の配列を定義してください
+- 数値型の配列を引数に取り、その最大値の数値型を返す関数を定義してください。*/
+//配列の要素を比較して、最も大きな数値を返す
+//数値の配列numbersを引数、number型の最大値を返す。
+//配列が空の場合は、Not a Numberを返す
+//配列内の各要素を順番に比較する
+//配列の中に、より大きい値が見つかる度にmaxを更新する
+//maxに格納された最大値を返す
+
+function findMax(numbers: number[]): number {
+  if (numbers.length === 0) {
+    return NaN; 
+  }
+
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+
+/*2. FizzBuzz 関数
+
+- 数値型を引数に取り、
+3の倍数のときには `Fizz` 
+5の倍数のときには `Buzz` 
+3と5の倍数のときには `FizzBuzz` 
+それ以外のときには元の数値を返す関数"string"を定義する
+- 上記の関数を使って、1～100までの数値の返り値を順番にコンソールに出力してください。*/
+
+function string(num: number): string {
+  if (num % 3 === 0) return "Fizz";
+  if (num % 5 === 0) return "Buzz";
+  if (num % 15 === 0) return "FizzBuzz";
+  return num.toString();
+}
+
+for (let i = 1; i <= 100; i++) {
+  console.log(string(i));
+}
+
+
+/*3. 逆さ文字列
+
+- 文字列を引数に取り、受け取った文字列を逆順にして返す関数を作成してください。
+- `baseball` という引数を与えたとき `llabesab` という文字列が返ることを確認してください。 */
+
+/*4. 文字列分割
+
+- 「任意の文章」と「区切り文字」を引数にとり、任意の文章を区切り文字で区切った配列を返す関数を作成してください。
+- `'apple,orange,strawberry'` という文章と `,` という区切り文字を与えたとき `['apple', 'orange', 'strawberry']` という配列が返ることを確認してください。 */
